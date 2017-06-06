82a44934d95bc9ee7c3abc11dabc84c443ba736b
Michael Dunlap <sagaquisces@gmail.com>
2017-06-05 21:41:42 -0700

    add if statement to prepend when suit is club and diamonds to reflect current unshuffled deck practice. still needs work.

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,18 @@ $(document).ready(function() {
   var ranks = ['Ace','2','3','4','5','6','7','8','9','10','Jack','Queen','King'];
   var suits = ['clubs','diamonds','spades','hearts'];
 
-  // var cards = [];
 
   suits.forEach(function(suit) {
   	ranks.forEach(function(rank) {
-      $("ul").append('<li class="' + suit + '">' + rank + " of " + suit + "</li>");
-    	// cards.push(rank + " of " + suit);
+
+      if (suit === "clubs" || suit === "diamonds") {
+        $("ul").append('<li class="' + suit + '">' + rank + ' of ' + suit + '</li>');
+      } else {
+
+        $("ul").prepend('<li class="' + suit + '">' + rank + ' of ' + suit + '</li>');
+      };
+
+
     });
   });
 
